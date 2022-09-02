bdce67de3d4fcfd73930d4bcbada1d5e6933b690
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2022-09-02 19:12:57 +0530

    Added feature buttons

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('HI');
+var circle=document.getElementById('circle');
+var upBtn=document.getElementById('upBtn');
+var downBtn=document.getElementById('downBtn');
+
+var rotateVal=circle.style.transform;
+var rotateSum;
+
+upBtn.addEventListener('click',()=>{
+    rotateSum= rotateVal + 'rotate(-90deg)';
+    circle.style.transform=rotateSum;
+    rotateVal=rotateSum;
+});
+downBtn.addEventListener('click',()=>{
+    rotateSum= rotateVal + 'rotate(90deg)';
+    circle.style.transform=rotateSum;
+    rotateVal=rotateSum;
+});
